5a979d0221b2e50d7bbf4f02ca6b0045acf92b47
Peaceful James <james@peacefulprogramming.dev>
2021-09-19 23:47:49 +0100

    Remove dupe TODO

--- lib/crypto_prices/coinbase/worker.ex
+++ lib/crypto_prices/coinbase/worker.ex
@@ -10,7 +10,6 @@ defmodule Crypto.Coinbase.Worker do
   # TODO @peaceful-james move DB job to task in context module
   # TODO @peaceful-james make interval and currency configurable
   # TODO @peaceful-james add specs throughout (use credo)
-  # TODO @peaceful-james add specs throughout (use credo)
 
   @prices_topic "prices"
   @interval :timer.seconds(5)
